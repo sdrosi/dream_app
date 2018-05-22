6f17c466a6aca6230a6f19d09d6ce3270b47b564
Philippe Recto <precto1285@gmal.com>
2018-05-22 09:25:27 -0700

    added to routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/", function (req, res) {
+  app.get("/api", function (req, res) {
     // Find one Author with the id in req.params.id and return them to the user with res.json
     db.User.findOne({
       where: {
@@ -19,7 +19,7 @@ module.exports = function (app) {
     });
   });
 
-  app.post("/", function (req, res) {
+  app.post("/api/add", function (req, res) {
     // Create an Author with the data available to us in req.body
     console.log(req.body);
     db.User.create(req.body).then(function (dbUser) {
@@ -27,7 +27,7 @@ module.exports = function (app) {
     });
   });
 
-  app.delete("/", function (req, res) {
+  app.delete("/api/delete", function (req, res) {
     // Delete the Author with the id available to us in req.params.id
     db.User.destroy({
       where: {
