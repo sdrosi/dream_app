9d729df17320bc9df41673ac3bf5fc692e79d9e8
Philippe Recto <precto1285@gmal.com>
2018-05-22 21:10:40 -0700

    added info to dreamsfeed.js and mydreams.js

--- public/js/dreamsfeed.js
+++ public/js/dreamsfeed.js
@@ -1,2 +1,10 @@
-//this is a thing
-var a
+$(document).ready(function() {
+    //dream container that holds all dreams posted.
+    var dreamContainer = $(".dream-container");
+    var privacySetting = $("#privacy-setting");
+    //click events for the edit and delete button
+    $(document).on("click", "button.delete", handleDreamsDelete);
+    $(document).on("click", "button.edit", handleDreamsEdit);
+    privacySetting.on("change", handleCategoryChange);
+
+});
