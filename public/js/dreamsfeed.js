dd963242ad9350c92734ba1303e99480cfc8da42
Philippe Recto <precto1285@gmal.com>
2018-05-24 11:24:11 -0700

    updated public js folder

--- public/js/dreamsfeed.js
+++ public/js/dreamsfeed.js
@@ -1,33 +1,57 @@
 $(document).ready(function () {
-    //dream container that holds all dreams posted.
-    var dreamContainer = $(".dream-container");
-    var privacySetting = $("#privacy-setting");
-    //click events for the edit and delete button
-    $(document).on("click", "button.delete", handleDreamsDelete);
-    $(document).on("click", "button.edit", handleDreamsEdit);
-    privacySetting.on("change", handleCategoryChange);
+  //dream container that holds all dreams posted.
+  var dreamContainer = $(".dream-container");
+  var privacySetting = $("#privacy-setting");
+  //click events for the edit and delete button
+  $(document).on("click", "button.delete", handleDreamsDelete);
+  $(document).on("click", "button.edit", handleDreamsEdit);
+  privacySetting.on("change", handleCategoryChange);
 
-    // This function grabs posts from the database and updates the view
+  // This function grabs dreams from the database and updates the view
+  function getDreams(category) {
 
-    // This function does an API call to delete posts
+  }
 
-    // Getting the initial list of posts
 
-    // InitializeRows handles appending all of our constructed post HTML inside
-    // blogContainer
+  // This function does an API call to delete dreamss
+  function deleteDream(id) {
 
-    // This function constructs a post's HTML
+  }
+  // Getting the initial list of dreams
+  getDreams();
 
-    // This function figures out which post we want to delete and then calls
-    // deletePost
+  // InitializeRows handles appending all of our constructed post HTML inside
+  // blogContainer
+  function initializeRows() {
 
-    // This function figures out which post we want to edit and takes it to the
+  }
+
+  // This function constructs a dream's HTML
+  function createNewRow(dream) {
+
+  }
+
+  // This function figures out which dream we want to delete and then calls
+  // deletePost
+  function handleDreamDelete() {
+
+  }
+  // This function figures out which dream we want to edit and takes it to the
   // Appropriate url
+  function handleDreamEdit() {
 
-  // This function displays a message when there are no posts
+  }
 
-  // This function handles reloading new posts when the category changes
+  // This function displays a message when there are no dreams
+  function displayEmpty() {
 
+  }
+
+  // This function handles reloading new dreams when the category changes
+  function handleCategoryChange() {
+
+  }
 
 
 });
+
