c26b3c95c4d34daa4bdf9e83ab176cb9fd952806
Shane Drosi <ShaneDrosi@Dobbie.jetpack>
2018-05-30 15:42:08 -0700

    updated to include mood on dreamsfeed and mydreams

--- public/js/mydreams.js
+++ public/js/mydreams.js
@@ -97,6 +97,10 @@ $(document).ready(function () {
     newDreamPolarityConfidence.text("Polarity Confidence: " + formattedPolCon + "%");
     newDreamPolarityConfidence.addClass("polarity_confidence")
 
+    var dreamMood = $("<td>");
+    dreamMood.text("Mood: " + dream.mood);
+    dreamMood.addClass("newDreamMood");
+
     var newDreamCategory = $("<td>");
     newDreamCategory.text("Dream Privacy: " + dreamPrivacy);
     newDreamCategory.addClass("dream-category")
@@ -124,6 +128,7 @@ $(document).ready(function () {
     newDreamCardHeading.append(newDreamDate);
     newDreamCardHeading.append(newDreamPolarity);
     newDreamCardHeading.append(newDreamPolarityConfidence);
+    newDreamCardHeading.append(dreamMood);
 
 
     newDreamCardBody.append(newDreamTitle);
